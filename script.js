fad52f8862b93c128e6259c4705f34c975f11a09
danielcollis <daniel.collis13@gmail.com>
2024-10-14 18:53:09 -0400

    pkmn selector menus

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 //identifes the dropdown menu and sprite image
 const dropdown = document.getElementById("spriteMenu");
+const pkmn1 = document.getElementById("pkmn1");
 const spriteImage = document.getElementById("sprite");
 
 //retrives pokemon names and adds them to the drop down menu
@@ -13,9 +14,48 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=156&offset=493")
     .then(data => data.results)
     .then(results => {for (let i = 0; i < results.length; i++) {
         const newOption = document.createElement("option");
-        newOption.value = results[i].name;
-        newOption.text = results[i].name;
-        dropdown.add(newOption);
+            //create new option for dropdown
+            newOption.value = results[i].name;
+            newOption.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption1 = document.createElement("option");
+            newOption1.value = results[i].name;
+            newOption1.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption2 = document.createElement("option");
+            newOption2.value = results[i].name;
+            newOption2.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption3 = document.createElement("option");
+            newOption3.value = results[i].name;
+            newOption3.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption4 = document.createElement("option");
+            newOption4.value = results[i].name;
+            newOption4.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption5 = document.createElement("option");
+            newOption5.value = results[i].name;
+            newOption5.text = results[i].name;
+
+            // Create another new option for pkmn1
+            const newOption6 = document.createElement("option");
+            newOption6.value = results[i].name;
+            newOption6.text = results[i].name;
+
+            // Add the options to the respective dropdowns
+            dropdown.add(newOption);
+            pkmn1.add(newOption1);
+            pkmn2.add(newOption2);
+            pkmn3.add(newOption3);
+            pkmn4.add(newOption4);
+            pkmn5.add(newOption5);
+            pkmn6.add(newOption6);
     }})
     .catch(error => console.log(error));
 
