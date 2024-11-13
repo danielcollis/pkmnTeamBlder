e66d644349bda26b695040bbd0db03e151e8ca27
danielcollis <daniel.collis13@gmail.com>
2024-11-13 14:59:23 -0500

    changed colors of stat bars

--- dex.js
+++ dex.js
@@ -1,3 +1,5 @@
+let bars = ["hpBar", "atkBar", "defBar", "spatkBar", "spdefBar", "speedBar"];
+
 function loadDex() {
     for (let i = 1; i <= 6; i++) {
         let pkmnName = localStorage.getItem(`pkmn${i}Name`);
@@ -9,6 +11,26 @@ function loadDex() {
                 screenCurr.children[0].src = data.sprites.front_default;
                 for (let j = 0; j < 6; j++) {
                     screenCurr.children[1].children[j].innerHTML += data.stats[j].base_stat;
+                    let currBarWidth = screenCurr.querySelector(`.${bars[j]}`).style.width = `${data.stats[j].base_stat * 1.2}px`
+                    let currBar = screenCurr.children[2].children[j].children[0];
+                    if ((parseFloat(currBarWidth) / 1.2) < 40) {
+                        currBar.style.backgroundColor = "red";
+                    }
+                    else if ((parseFloat(currBarWidth) / 1.2) < 60) {
+                        currBar.style.backgroundColor = "orangered";
+                    }
+                    else if ((parseFloat(currBarWidth) / 1.2) < 80) {
+                        currBar.style.backgroundColor = "orange";
+                    }
+                    else if ((parseFloat(currBarWidth) / 1.2) < 100) {
+                        currBar.style.backgroundColor = "yellow";
+                    }
+                    else if ((parseFloat(currBarWidth) / 1.2) < 120) {
+                        currBar.style.backgroundColor = "yellowgreen";
+                    }
+                    else {
+                        currBar.style.backgroundColor = "limegreen";
+                    }
                 }
             })
             .catch(error => console.error(error));
