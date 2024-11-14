c6ffbe4afb62c671afee1fee004ce64f7d9c6501
danielcollis <daniel.collis13@gmail.com>
2024-11-13 21:49:08 -0500

    added caption to images on dex page and increased stat bar size

--- dex.js
+++ dex.js
@@ -2,30 +2,38 @@ let bars = ["hpBar", "atkBar", "defBar", "spatkBar", "spdefBar", "speedBar"];
 
 function loadDex() {
     for (let i = 1; i <= 6; i++) {
+        //gets selected names from local storage
         let pkmnName = localStorage.getItem(`pkmn${i}Name`);
+        //iterates through all divs on the screen
         let screenCurr = document.getElementById(`screen${i}`);
         if (pkmnName != "None") {
             fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnName}`)
             .then(response => response.json())
             .then(data => {
-                screenCurr.children[0].src = data.sprites.front_default;
+                //sets the img attribute in each figure to the sprite from pokeapi
+                screenCurr.children[0].children[0].src = data.sprites.front_default;
+                //sets capitalName to a capitalized version of the selected names
+                let capitalName = pkmnName.charAt(0).toUpperCase() + pkmnName.slice(1).toLowerCase();
+                //changes the image caption to the capitalized string
+                screenCurr.children[0].children[1].innerHTML = capitalName;
+                //iterates through each
                 for (let j = 0; j < 6; j++) {
                     screenCurr.children[1].children[j].innerHTML += data.stats[j].base_stat;
-                    let currBarWidth = screenCurr.querySelector(`.${bars[j]}`).style.width = `${data.stats[j].base_stat * 1.2}px`
+                    let currBarWidth = screenCurr.querySelector(`.${bars[j]}`).style.width = `${data.stats[j].base_stat * 1.6}px`
                     let currBar = screenCurr.children[2].children[j].children[0];
-                    if ((parseFloat(currBarWidth) / 1.2) < 40) {
+                    if ((parseFloat(currBarWidth) / 1.6) < 40) {
                         currBar.style.backgroundColor = "red";
                     }
-                    else if ((parseFloat(currBarWidth) / 1.2) < 60) {
+                    else if ((parseFloat(currBarWidth) / 1.6) < 60) {
                         currBar.style.backgroundColor = "orangered";
                     }
-                    else if ((parseFloat(currBarWidth) / 1.2) < 80) {
+                    else if ((parseFloat(currBarWidth) / 1.6) < 80) {
                         currBar.style.backgroundColor = "orange";
                     }
-                    else if ((parseFloat(currBarWidth) / 1.2) < 100) {
+                    else if ((parseFloat(currBarWidth) / 1.6) < 100) {
                         currBar.style.backgroundColor = "yellow";
                     }
-                    else if ((parseFloat(currBarWidth) / 1.2) < 120) {
+                    else if ((parseFloat(currBarWidth) / 1.6) < 120) {
                         currBar.style.backgroundColor = "yellowgreen";
                     }
                     else {
